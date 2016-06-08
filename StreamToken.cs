190db25f39b323991cfa1438301d58ec57281653
Ricardo <rlamasb@gmail.com>
2016-06-08 14:05:43 +1000

    Changes to forward the Key for the object created.

--- StreamToken.cs
+++ StreamToken.cs
@@ -26,8 +26,8 @@
 			return this;
 		}
 
-		public void Subscribe(Action<T> onDataAdded)
-			=> _observableDisposable = _observable.Subscribe(d => onDataAdded(d.Object));
+		public void Subscribe(Action<T, string> onDataAdded)
+			=> _observableDisposable = _observable.Subscribe(d => onDataAdded(d.Object, d.Key));
 
 		public void Dispose()
 		{
