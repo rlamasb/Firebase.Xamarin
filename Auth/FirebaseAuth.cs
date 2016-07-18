5045f3cf32b7135e4d29511ec3f357dbe57b6d07
Jacob Morris <jacob@madmediagroup.com.au>
2016-07-18 16:55:38 +1000

    #1 - Implemented Firebase Authentication based on https://github.com/step-up-labs/firebase-authentication-dotnet

---
+++ Auth/FirebaseAuth.cs
@@ -0,0 +1,49 @@
+﻿namespace Firebase.Xamarin
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The firebase auth.
+    /// </summary>
+    public class FirebaseAuth
+    {
+        /// <summary>
+        /// Gets or sets the firebase token which can be used for authenticated queries. 
+        /// </summary>
+        [JsonProperty("idToken")]
+        public string FirebaseToken
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the refresh token of the underlying service which can be used to get a new access token. 
+        /// </summary>
+        [JsonProperty("refreshToken")]
+        public string RefreshToken
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the numbers of seconds until the token expires.
+        /// </summary>
+        [JsonProperty("expiresIn")]
+        public int ExpiresIn
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the user.
+        /// </summary>
+        public User User
+        {
+            get;
+            set;
+        }
+    }
+}
