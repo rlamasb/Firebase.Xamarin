72b241ae1cc8c10eae6d6422dd80bee905e25d84
José Otávio Quaglio <joseotavio@gmail.com>
2016-11-18 10:24:57 -0200

    Included another ctor for push notification key

--- Auth/FirebaseConfig.cs
+++ Auth/FirebaseConfig.cs
@@ -1,4 +1,4 @@
-﻿namespace Firebase.Xamarin.Auth
+namespace Firebase.Xamarin.Auth
 {
     /// <summary>
     /// The auth config. 
@@ -13,11 +13,32 @@
         {
             this.ApiKey = apiKey;
         }
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:FirebaseConfig"/> class.
+        /// </summary>
+        /// <param name="apiKey">API key.</param>
+        /// <param name="apiKeyForPushNotification">API key for push notification.</param>
+        public FirebaseConfig(string apiKey, string apiKeyForPushNotification)
+        {
+            this.ApiKey = apiKey;
+            this.ApiKeyForPushNotification = apiKeyForPushNotification;
+        }
 
         /// <summary>
         /// Gets or sets the api key of your Firebase app. 
         /// </summary>
         public string ApiKey 
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the API key for push notification.
+        /// </summary>
+        /// <value>The API key for push notification.</value>
+        public string ApiKeyForPushNotification
         {
             get;
             set;
